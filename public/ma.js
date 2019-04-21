a38f927295b55484908c2f148d564114d797c299
NurTur <turgynbekov.nurbolat.nurlanovich@gmail.com>
2019-04-21 17:40:03 +0600

    full pack

---
+++ public/ma.js
@@ -0,0 +1,39 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev');
+
+console.log("ggggg", isDev);
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+
+    mainWindow.loadURL(isDev ? 'http://localhost:8080' : __dirname + '../dist/index.html');
+
+    mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
